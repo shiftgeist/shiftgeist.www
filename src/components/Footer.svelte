d4837273f713555bdd26e4d1a1ed44c86a586b90
shiftgeist <git@shiftgeist.com>
2020-05-07 02:08:24 +0200

    Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+  const year = new Date().getFullYear()
+</script>
+
+<footer class="flex items-center justify-between py-8 mt-16 text-gray-500">
+  <span>©{year} Felix Hungenberg</span>
+
+  <div class="text-lg">
+    <a class="ml-2" href="mailto:shiftgeist@gmail.com">E</a>
+    <a class="ml-2" href="https://twitter.com/shiftgeist" target="_blank" rel="noopener noreferrer">T</a>
+    <a class="ml-2" href="https://linkedin.com/in/fhungenberg/" target="_blank" rel="noopener noreferrer">L</a>
+    <a class="ml-2" href="https://dribbble.com/shiftgeist" target="_blank" rel="noopener noreferrer">D</a>
+    <a class="ml-2" href="https://github.com/shiftgeist" target="_blank" rel="noopener noreferrer">G</a>
+  </div>
+</footer>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,16 @@
+<script>
+// Hide on scrolldown
+// Show on scrollup
+</script>
+
+<nav class="container flex items-center justify-between py-8 mx-auto">
+  <a href="/">
+    <img src="assets/icon-white.svg" alt="Logo" class="w-16">
+  </a>
+
+  <ul class="flex text-lg">
+    <li class="ml-4 hover:text-blue-500"><a href="#projects">Projects</a></li>
+    <li class="ml-4 hover:text-blue-500"><a href="#blog">Blog</a></li>
+    <li class="ml-4 hover:text-blue-500"><a href="#about">About</a></li>
+  </ul>
+</nav>
