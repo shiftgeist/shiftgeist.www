ea66ab018a6de4502b3cea0514f0cf6eca391839
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2020-05-07 02:08:24 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Navigation from './components/Navigation.svelte';
+  import Footer from './components/Footer.svelte';
+</script>
+
+<Navigation/>
+
+<header class="my-32 xl:my-56">
+	<h1 class="font-serif text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+    <p class="text-red-600">Hi! I'm Felix.</p>
+    <p>An ui engineer @ <a href="#hosting.de" class="underline hover:text-blue-500">hosting.de</a> working on the future of hosting.</p>
+  </h1>
+</header>
+
+<main>
+  <div class="mb-32 text-gray-500">You shall scroll down</div>
+
+  <!-- #projects -->
+
+  <!-- #blog -->
+
+  <!-- About -->
+  <section>
+    <h2 class="mb-1 font-serif" id="about">About</h2>
+    <p class="mb-6">Es war einmal...</p>
+
+    <div class="mb-8">
+      <h3 class="mb-1 font-serif" id="contact">Get In Touch</h3>
+      <p class="mb-2">You can contact me via <a href="mailto:shiftgeist@gmail.com" class="underline">email</a>, or reach out thought <a href="https://twitter.com/shiftgeist" target="_blank" rel="noopener noreferrer" class="underline">Twitter</a>.</p>
+      <p class="mb-2">
+        <a href="https://linkedin.com/in/fhungenberg/" target="_blank" rel="noopener noreferrer" class="mr-4 underline">Linkedin</a>
+        <a href="https://dribbble.com/shiftgeist" target="_blank" rel="noopener noreferrer" class="mr-4 underline">Dribbble</a>
+        <a href="https://github.com/shiftgeist" target="_blank" rel="noopener noreferrer" class="mr-4 underline">Github</a>
+      </p>
+    </div>
+
+    <div class="mb-8">
+      <h3 class="mb-1 font-serif" id="stack">Stack</h3>
+      <p class="mb-2">This website is handcrafted with the unconventional svelte. It uses both Asap and Merriweather typefaces and an awesome icon library called <a href="https://feathericons.com/" target="_blank" rel="noopener noreferrer">feather</a> icons.</p>
+      <p class="mb-2">It gets its flair thought tailwind and its speed from the essential purgecss.</p>
+    </div>
+  </section>
+</main>
+
+<Footer/>
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Asap', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    colors: theme => ({
+      black: '#000000',
+      white: '#ffffff',
+      gray: {
+        100: '#f0eded', // FG
+        200: '#e3e1e1',
+        300: '#d1cfcf',
+        400: '#acaaaa',
+        500: '#8b8989', // Muted
+        600: '#646262',
+        700: '#514f4f',
+        800: '#333131',
+        900: '#131111', // BG
+      },
+      red: {
+        100: '#ffcdd2',
+        200: '#ef9a9a',
+        300: '#e57373',
+        400: '#ef5250',
+        500: '#f44236',
+        600: '#e53935', // Main
+        700: '#d32e2f',
+        800: '#c62728',
+        900: '#b71a1c',
+      },
+      blue: {
+        100: '#baddf7',
+        200: '#8fc8f2',
+        300: '#63b2ec',
+        400: '#41a2e9',
+        500: '#1E92E6', // Main
+        600: '#1785d9',
+        700: '#0e73c6',
+        800: '#0663b4',
+        900: '#004596',
+      },
+    }),
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    content: [
+      './public/**/*.html',
+      './src/**/*.svelte',
+    ],
+    options: {},
+  },
+};
