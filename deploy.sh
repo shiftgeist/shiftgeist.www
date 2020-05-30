de979bcf9af9593125179a86af803998da43ab6e
shiftgeist <git@shiftgeist.com>
2020-05-30 14:40:54 +0200

    build: add compression to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 eval $(egrep -v '^#' .env | xargs)
-rsync -avz --delete -e "ssh -p $WS_PORT" $WS_SOURCE $WS_USER@$WS_HOST:~/$WS_DIR
+rsync -avzz --delete -e "ssh -p $WS_PORT" $WS_SOURCE $WS_USER@$WS_HOST:~/$WS_DIR
