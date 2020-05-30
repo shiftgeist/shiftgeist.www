4899db0371208633af7a5f5115fae3dcfadfc4f4
shiftgeist <git@shiftgeist.com>
2020-05-30 16:22:17 +0200

    feat: add basics

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,39 +1,69 @@
 <script>
-  const year = new Date().getFullYear();
+  import { darkTheme } from "../stores";
+  import FooterItem from "./FooterItem.svelte";
+  import { icons } from "feather-icons";
+
+  let legalOpen = false;
 </script>
 
 <footer class="flex items-center justify-between py-8 mt-16 text-gray-500">
-  <span>©{year} Felix Hungenberg</span>
+  <span>©{new Date().getFullYear()} Felix Hungenberg</span>
 
-  <div class="text-lg">
-    <a class="ml-2" href="mailto:shiftgeist@gmail.com">M</a>
-    <a
-      class="ml-2"
-      href="https://twitter.com/shiftgeist"
-      target="_blank"
-      rel="noopener noreferrer">
-      T
-    </a>
-    <a
-      class="ml-2"
-      href="https://linkedin.com/in/fhungenberg/"
-      target="_blank"
-      rel="noopener noreferrer">
-      L
-    </a>
-    <a
-      class="ml-2"
-      href="https://dribbble.com/shiftgeist"
-      target="_blank"
-      rel="noopener noreferrer">
-      D
-    </a>
-    <a
-      class="ml-2"
-      href="https://github.com/shiftgeist"
-      target="_blank"
-      rel="noopener noreferrer">
-      G
-    </a>
+  <div class="-m-2 text-lg">
+    <span class="relative">
+      <button
+        class="p-2 mr-4 pointer"
+        on:click={() => (legalOpen = !legalOpen)}>
+        Legal
+      </button>
+      {#if legalOpen}
+        <div
+          class="absolute mb-12 rounded shadow-lg left-1/2 bottom-0 p-4 pr-20
+          transform -translate-x-1/2 modal {$darkTheme ? 'bg-gray-800 text-white' : ' bg-white text-black'}">
+          <div class="flex justify-between">
+            <h1 class="mb-2 font-serif text-xl">Impressum</h1>
+            <button
+              on:click={() => (legalOpen = false)}
+              class="absolute top-0 right-0 p-4">
+              {@html icons.x.toSvg()}
+            </button>
+          </div>
+          <p class="whitespace-no-wrap">Felix Hungenberg</p>
+          <p class="whitespace-no-wrap">Herrenstr. 68 A</p>
+          <p class="whitespace-no-wrap">50170 Kerpen</p>
+        </div>
+      {/if}
+    </span>
+
+    <FooterItem link="mailto:hi@shiftgeist.com" name="Mail">
+      <span>M</span>
+      <span slot="entered">
+        {@html icons.mail.toSvg({ class: 'inline' })}
+      </span>
+    </FooterItem>
+    <FooterItem link="https://twitter.com/shiftgeist" name="Tweets">
+      <span>T</span>
+      <span slot="entered">
+        {@html icons.twitter.toSvg({ class: 'inline' })}
+      </span>
+    </FooterItem>
+    <FooterItem link="https://linkedin.com/in/fhungenberg" name="Connect">
+      <span>L</span>
+      <span slot="entered">
+        {@html icons.linkedin.toSvg({ class: 'inline' })}
+      </span>
+    </FooterItem>
+    <FooterItem link="https://dribbble.com/shiftgeist" name="Dribbble">
+      <span>D</span>
+      <span slot="entered">
+        {@html icons.play.toSvg({ class: 'inline' })}
+      </span>
+    </FooterItem>
+    <FooterItem link="https://github.com/shiftgeist" name="GitHub">
+      <span>G</span>
+      <span slot="entered">
+        {@html icons.github.toSvg({ class: 'inline' })}
+      </span>
+    </FooterItem>
   </div>
 </footer>
---
+++ src/components/FooterItem.svelte
@@ -0,0 +1,20 @@
+<script>
+  let entered = false;
+  export let link;
+  export let name;
+</script>
+
+<a
+  class="inline-flex justify-center w-10 p-2"
+  href={link}
+  target="_blank"
+  rel="noopener noreferrer"
+  title={name}
+  on:mouseenter={() => (entered = true)}
+  on:mouseleave={() => (entered = false)}>
+  {#if entered}
+    <slot name="entered" class.hidden={entered} />
+  {:else}
+    <slot />
+  {/if}
+</a>
