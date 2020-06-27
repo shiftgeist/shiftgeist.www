61d4036d61aa7749f0c0f57ef54baefb880e5152
shiftgeist <git@shiftgeist.com>
2020-06-27 19:58:09 +0200

    add lazyloading for images

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import App from './App.svelte';
-import './main.css';
+import App from "./App.svelte";
+import "./main.css";
+import "lazysizes";
 
 const app = new App({
-	target: document.body,
-	props: {}
+  target: document.body,
+  props: {},
 });
 
 export default app;
