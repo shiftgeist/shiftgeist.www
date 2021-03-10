7ae2ad420dbfc85c061c81a3c43d90117dde8c44
shiftgeist <git@shiftgeist.com>
2021-03-11 00:15:16 +0100

    fix workflow node version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-rsync -avzz --delete -e "ssh -p $WS_PORT" "$WS_SOURCE" "$WS_USER@$WS_HOST:~/$WS_DIR"
+set -eu
+
+# Set deploy key
+SSH_PATH="$HOME/.ssh"
+mkdir -p "$SSH_PATH"
+echo "$SSH_KEY" > "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key"
+
+# Do deployment
+rsync -avzz --delete -e "ssh -p $WS_PORT -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no" "$WS_SOURCE" "$SSH_USER@$WS_HOST:~/$WS_HOME_DIR"
