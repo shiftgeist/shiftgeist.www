410e379327378d6fa20b89ca0ee98a02d840e393
shiftgeist <git@shiftgeist.com>
2021-03-18 14:55:33 +0100

    add storybook

---
+++ docs/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../stories/**/*.stories.svelte"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf",
+  ],
+};
