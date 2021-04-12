efc563528f95690b05f1800dd12a0b07de8e4816
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2021-04-12 03:16:05 +0200

    initialise svelte kit

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,24 +1,13 @@
-<!-- https://github.com/sveltejs/kit/blob/master/examples/hn.svelte.dev/src/routes/%24layout.svelte -->
 <script>
-  import { onMount } from "svelte";
-  import { theme, themes } from "../stores/theme";
-  import "virtual:windi.css";
-  // import "lazysizes"; // with images
+  import '../app.css';
+  import '../services/i18n';
 
-  onMount(() => {
-    const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-    theme.set(darkTheme ? themes.dark : themes.light);
-
-    darkTheme.addEventListener("change", (e) => {
-      theme.set(e.matches ? themes.dark : themes.light);
-      document.body.classList.toggle(themes.dark);
-    });
-  });
+  import Navigation from '$lib/Navigation.svelte';
+  import Footer from '$lib/Footer.svelte';
 </script>
 
-<svelte:body class={$theme} />
+<Navigation />
 
-<main>
-  <slot />
-</main>
+<slot />
+
+<Footer />
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,17 +1,26 @@
-const sveltePreprocess = require("svelte-preprocess");
+const sveltePreprocess = require('svelte-preprocess');
+const static = require('@sveltejs/adapter-static');
+const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [sveltePreprocess()],
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [require('autoprefixer')()]
+    }
+  }),
   kit: {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: "@sveltejs/adapter-static",
+    adapter: static(),
 
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-  },
+    target: '#svelte',
+
+    vite: {
+      ssr: false
+    }
+  }
 };
